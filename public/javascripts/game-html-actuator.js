ef9c4ede2b148b63046de0168a689c4d1a66f2f9
Mako <mnfeconicu41@gmail.com>
2017-05-25 22:13:27 +0900

    keyboard input

--- public/javascripts/game-html-actuator.js
+++ public/javascripts/game-html-actuator.js
@@ -1,6 +1,55 @@
 
 class HTMLActuator {
   constructor() {
-    //
+    this.tileContainer        = $(".tile-container");
+    this.messageContainer     = $(".game-message");
+    this.tileRemovedContainer = $(".tile-removed-container");
+  }
+
+  actuate(grid, metadata) {
+    window.requestAnimationFrame(() => {
+      // clear
+      // add tiles
+      // remove tile if tile becomes pack or syringe
+      // show message if game is terminated
+    });
+  }
+
+  addTile(tile) {
+    var wrapper  = $("<div></div>");
+    var inner    = $("<div></div>");
+    var position = tile.previousPosition || { x: tile.x, y: tile.y };
+    var positionClass = this.cssPositionClass(position);
+
+    // Apply css class to wrapper and inner
+
+    if (tile.previousPosition) {
+      // Make sure that the tile gets rendered in the previous position first
+      // TODO:
+    } else if (tile.mergedFrom) {
+      // Render the tiles that merged
+      // TODO:
+    } else {
+      // TODO:
+    }
+
+    // Add the inner part of the tile to the wrapper
+    wrapper.append(inner);
+
+    // Put the tile on the board
+    this.tileContainer.append(wrapper);
+  }
+
+  cssPositionClass(position) {
+    position = { x: position.x + 1, y: position.y + 1 };
+    return "tile-position-" + position.x + "-" + position.y;
+  }
+
+  message(won) {
+    var type    = won ? "game-won" : "game-over";
+    var message = won ? "You win!" : "Game over!";
+
+    this.messageContainer.addClass(type);
+    this.messageContainer.find("p")[0].textContent = message;
   }
 }
--- public/javascripts/game-manager.js
+++ public/javascripts/game-manager.js
@@ -7,10 +7,170 @@ class GameManager {
     this.actuator     = new Actuator(color);
     this.startTiles   = 2;
 
+    this.inputManager.on("move", this.move.bind(this));
+    this.inputManager.on("restart", this.restart.bind(this));
+
     this.setup();
   }
 
+  restart() {
+    // clear message
+    this.setup();
+  }
+
+  isGameTerminated() {
+    return this.over || this.won;
+  }
+
   setup() {
-    //
+    this.grid  = new Grid(this.size);
+    this.score = 0;
+    this.over  = false;
+    this.won   = false;
+
+    // Add the initial tiles
+    for (var i = 0; i < this.startTiles; i++) {
+      this.addRandomTile();
+    }
+
+    // Update the actuator
+    this.actuate();
+  }
+
+  // Adds a tile in a random position
+  addRandomTile() {
+    // TODO:
+  }
+
+  // Sends the updated grid to the actuator
+  actuate() {
+    // TODO:
+  }
+
+  // Save all tile positions and remove merger info
+  prepareTiles() {
+    // TODO:
+  }
+
+  // Move tiles on the grid in the specified direction
+  move(direction) {
+    // 0: up, 1: right, 2: down, 3: left
+
+    if (this.isGameTerminated()) return;
+
+    var vector     = this.getVector(direction);
+    var traversals = this.buildTraversals(vector);
+    var moved      = false;
+
+    console.log(vector);
+
+    // Save the current tile positions and remove merger information
+    this.prepareTiles();
+
+    // Traverse the grid in the right direction and move tiles
+
+    // Extract a packed knowledge and a vaccine
+
+    if (moved) {
+      this.addRandomTile();
+
+      if (!this.movesAvailable()) {
+        this.over = true;
+      }
+
+      this.actuate();
+    }
+  }
+
+  // Get the vector representing the chosen direction
+  getVector(direction) {
+    var map = {
+      0: { x: 0,  y: -1 }, // Up
+      1: { x: 1,  y: 0 },  // Right
+      2: { x: 0,  y: 1 },  // Down
+      3: { x: -1, y: 0 }   // Left
+    };
+    return map[direction];
+  }
+
+  // Build a list of positions to traverse in the right order
+  // ex)
+  //     when vector = ( 0, -1) // Up
+  //       return { x: [0,1,2,3], y: [0,1,2,3] }
+  //     when vector = ( 1,  0) // Right
+  //       return { x: [3,2,1,0], y: [0,1,2,3] }
+  //     when vector = ( 0,  1) // Down
+  //       return { x: [0,1,2,3], y: [3,2,1,0] }
+  //     when vector = ( 1,  0) // Left
+  //       return { x: [0,1,2,3], y: [0,1,2,3] }
+  //
+  buildTraversals(vector) {
+    var traversals = { x: [], y: [] };
+
+    for (var pos = 0; pos < this.size; pos++) {
+      traversals.x.push(pos);
+      traversals.y.push(pos);
+    }
+    // Always traverse from the farthest cell in the chosen direction
+    if (vector.x === 1) traversals.x = traversals.x.reverse();
+    if (vector.y === 1) traversals.y = traversals.y.reverse();
+    return traversals;
+  }
+
+  findFarthestPosition(cell, vector) {
+    var previous;
+
+    // Progress towards the vector direction until an obstacle is found
+    do {
+      previous = cell;
+      cell     = { x: previous.x + vector.x, y: previous.y + vector.y };
+    } while (this.grid.withinBounds(cell) &&
+             this.grid.cellAvailable(cell));
+
+    return {
+      farthest: previous,
+      next: cell // Used to check if a merge is required
+    };
+  }
+
+  movesAvailable() {
+    return this.grid.hasEmptyCell() || this.tileMatchesAvailable();
+  }
+
+  // Check for available matches between tiles (more expensive check)
+  tileMatchesAvailable() {
+    var self = this;
+
+    for (var x = 0; x < this.size; x++) {
+      for (var y = 0; y < this.size; y++) {
+        var tile = this.grid.cellContent({ x: x, y: y });
+
+        if (tile) {
+          for (var direction = 0; direction < 4; direction++) {
+            var vector = self.getVector(direction);
+            var cell   = { x: x + vector.x, y: y + vector.y };
+            var other  = self.grid.cellContent(cell);
+
+            if (other && other.type === tile.type) {
+              return true; // These two tiles can be merged
+            }
+          }
+        }
+      }
+    }
+    return false;
   }
 }
+
+// Class propaties and class methods
+
+// {red: "pink", blue: "lightblue", green: "lightgreen", yellow: "yellow"}
+GameManager.colors    = ["red",  "blue",      "green",      "yellow"];
+GameManager.cssColors = ["pink", "lightblue", "lightgreen", "yellow"];
+GameManager.cssColorMap = (function () {
+  var hash = {};
+  for (var i = 0; i < GameManager.colors.length; i++) {
+    hash[GameManager.colors[i]] = GameManager.cssColors[i];
+  }
+  return hash;
+}());
