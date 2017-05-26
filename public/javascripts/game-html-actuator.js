4bd746f65d4b2da2101a68b51b5108fdd7a96c2c
Mako <mnfeconicu41@gmail.com>
2017-05-26 13:18:22 +0900

    animation bug when using jquery

--- public/javascripts/game-html-actuator.js
+++ public/javascripts/game-html-actuator.js
@@ -1,92 +1,151 @@
 
 class HTMLActuator {
   constructor() {
-    this.tileContainer        = $(".tile-container");
-    this.messageContainer     = $(".game-message");
-    this.tileRemovedContainer = $(".tile-removed-container");
+    // this.tileContainer        = $(".tile-container");
+    // this.messageContainer     = $(".game-message");
+    // this.tileRemovedContainer = $(".tile-removed-container");
+    this.tileContainer        = document.querySelector(".tile-container");
+    this.messageContainer     = document.querySelector(".game-message");
+    this.tileRemovedContainer = document.querySelector(".tile-removed-container");
   }
 
   actuate(grid, metadata) {
-    window.requestAnimationFrame(() => {
-      // clear
-      this.tileContainer.empty();
+    var self = this;
 
-      // add tiles
+    window.requestAnimationFrame(() => {
+      // Clear tile container
+      // self.tileContainer.empty();
+      while (self.tileContainer.firstChild) {
+        self.tileContainer.removeChild(self.tileContainer.firstChild);
+      }
+
       grid.cells.forEach((column) => {
         column.forEach((cell) => {
           if (cell) {
-            this.addTile(cell);
+            self.addTile(cell);
           }
         });
       });
 
-      // remove tile if tile becomes pack or syringe
-      // TODO:
+      grid.cells.forEach((column) => {
+        column.forEach((tile) => {
+          if (tile && tile.pack) {
+            grid.removeTile(tile); // update grid
+            self.removeTile(tile); // animate
+          }
+        });
+      });
 
-      // show message if game is terminated
       if (metadata.terminated) {
         if (metadata.over) {
-          this.message(false);
+          self.message(false); // You lose
         } else if (metadata.won) {
-          this.message(true);
+          self.message(true); // You win!
         }
       }
     });
   }
 
   addTile(tile) {
-    var wrapper  = $("<div></div>");
-    var inner    = $("<div></div>");
-    var position = tile.previousPosition || { x: tile.x, y: tile.y };
+    var self = this;
+
+    // var wrapper   = $("<div></div>");
+    // var inner     = $("<div></div>");
+    var wrapper   = document.createElement("div");
+    var inner     = document.createElement("div");
+    var position  = tile.previousPosition || { x: tile.x, y: tile.y };
     var positionClass = this.cssPositionClass(position);
 
-    // Apply css class to wrapper and inner
-    wrapper.addClass("tile");
+    // wrapper.addClass("tile");
+    // if (tile.syringe) {
+    //   wrapper.addClass("tile-" + tile.cssType + "-syringe");
+    // } else if (tile.pack) {
+    //   wrapper.addClass("tile-" + tile.cssType + "-pack");
+    // } else {
+    //   wrapper.addClass("tile-" + tile.cssType + "-" + tile.value);
+    // }
+    // wrapper.addClass(positionClass);
+    //
+    // inner.addClass("tile-inner");
+    // // inner.textContent = tile.value;
+
+    // We can't use classlist because it somehow glitches when replacing classes
+    var classes = ["tile"];
     if (tile.syringe) {
-      wrapper.addClass("tile-" + tile.cssType + "-syringe");
+      classes.push("tile-" + tile.cssType + "-syringe");
     } else if (tile.pack) {
-      wrapper.addClass("tile-" + tile.cssType + "-pack");
+      classes.push("tile-" + tile.cssType + "-pack");
     } else {
-      wrapper.addClass("tile-" + tile.cssType + "-" + tile.value);
+      classes.push("tile-" + tile.cssType + "-" + tile.value);
+    }
+    classes.push(positionClass);
+    this.applyClasses(wrapper, classes);
+
+    inner.classList.add("tile-inner");
+    // inner.textContent = tile.value;
+
+    if (tile.previousPosition) {
+      // Make sure that the tile gets rendered in the previous position first
+      window.requestAnimationFrame(function () {
+        // wrapper.addClass( self.cssPositionClass({ x: tile.x, y: tile.y }) );
+        classes[2] = self.cssPositionClass({ x: tile.x, y: tile.y });
+        self.applyClasses(wrapper, classes); // Update the position
+      });
+    } else if (tile.mergedFrom) {
+      // wrapper.addClass("tile-merged");
+      classes.push("tile-merged");
+      this.applyClasses(wrapper, classes);
+
+      // Render the tiles that merged
+      tile.mergedFrom.forEach(function (merged) {
+        self.addTile(merged);
+      });
+    } else {
+      // wrapper.addClass("tile-new");
+      classes.push("tile-new");
+      this.applyClasses(wrapper, classes);
     }
 
-    inner.addClass("tile-inner");
-
-    // if (tile.previousPosition) {
-    //   // Make sure that the tile gets rendered in the previous position first
-    //   window.requestAnimationFrame(() => {
-    //     wrapper.addClass(this.cssPositionClass({ x: tile.x, y: tile.y }));
-    //     // TODO: replace this^ code to dynamic positioning by js
-    //   });
-    // } else if (tile.mergedFrom) {
-    //   wrapper.addClass("tile-merged");
-    //
-    //   // Render the tiles that merged
-    //   tile.mergedFrom.forEach((merged) => {
-    //     this.addTile(merged);
-    //   });
-    // } else {
-      wrapper.addClass(this.cssPositionClass({ x: tile.x, y: tile.y }));
-      wrapper.addClass("tile-new");
-    // }
-
     // Add the inner part of the tile to the wrapper
-    wrapper.append(inner);
+    // wrapper.append(inner);
+    wrapper.appendChild(inner);
 
     // Put the tile on the board
-    this.tileContainer.append(wrapper);
+    // this.tileContainer.append(wrapper);
+    this.tileContainer.appendChild(wrapper);
   }
 
-  cssPositionClass(position) {
-    position = { x: position.x + 1, y: position.y + 1 };
-    return "tile-position-" + position.x + "-" + position.y;
+  applyClasses(element, classes) {
+    element.setAttribute("class", classes.join(" "));
+  }
+
+  cssPositionClass(pos) {
+    pos = { x: pos.x + 1, y: pos.y + 1 };
+    return "tile-position-" + pos.x + "-" + pos.y;
   }
 
   message(won) {
     var type    = won ? "game-won" : "game-over";
     var message = won ? "You win!" : "Game over!";
 
-    this.messageContainer.addClass(type);
-    this.messageContainer.find("p")[0].textContent = message;
+    this.messageContainer.classList.add(type);
+    this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+  }
+
+  clearMessage() {}
+
+  removeTile(pos) {
+    // $(".tile-container").find("." + this.cssPositionClass(pos)).each(function(index, elem) {
+    //   if ($(elem).hasClass("tile-merged")) {
+    //     $(elem).appendTo(".tile-removed-container");
+    //   } else {
+    //     $(elem).animate({ opacity: 0 }, 500).promise().then(function () { $(this).remove() })
+    //   }
+    // });
+    //
+    // $(".tile-removed-container .tile").each(function(index, elem) {
+    //   $(elem).css({ zIndex: "100", position: "fixed" })
+    //   $(elem).animate({ top: 0 }, 1000).animate({ opacity: 0 }, 300).promise().then(function () { $(this).remove() })
+    // })
   }
 }
--- public/javascripts/game-manager.js
+++ public/javascripts/game-manager.js
@@ -1,9 +1,9 @@
 
 class GameManager {
   constructor(color, InputManager, Actuator) {
-    this.size = 4; // Size of the grid
-    this.copeWith = color; // color will be virus color (red, blue, green, yellow)
-    this.inputManager = new InputManager();
+    this.size         = 4; // Size of the grid
+    this.copeWith     = color; // color will be virus color (red, blue, green, yellow)
+    this.inputManager = new InputManager;
     this.actuator     = new Actuator(color);
     this.startTiles   = 2;
 
@@ -13,15 +13,18 @@ class GameManager {
     this.setup();
   }
 
+  // Restart the game
   restart() {
-    // clear message
+    this.actuator.clearMessage(); // Clear the game won/lost message
     this.setup();
   }
 
+  // Return true if the game is lost, or has won
   isGameTerminated() {
     return this.over || this.won;
   }
 
+  // Set up the game
   setup() {
     this.grid  = new Grid(this.size);
     this.score = 0;
@@ -32,8 +35,6 @@ class GameManager {
     for (var i = 0; i < this.startTiles; i++) {
       this.addRandomTile();
     }
-
-    console.log(this.grid);
 
     // Update the actuator
     this.actuate();
@@ -61,36 +62,105 @@ class GameManager {
 
   // Save all tile positions and remove merger info
   prepareTiles() {
-    // TODO:
+    this.grid.eachCell(function (x, y, tile) {
+      if (tile) {
+        tile.mergedFrom = null;
+        tile.savePosition();
+      }
+    });
+  }
+
+  // Move a tile
+  moveTile(tile, cell) {
+    this.grid.cells[tile.x][tile.y] = null;
+    this.grid.cells[cell.x][cell.y] = tile;
+    tile.updatePosition(cell);
   }
 
   // Move tiles on the grid in the specified direction
   move(direction) {
     // 0: up, 1: right, 2: down, 3: left
+    var self = this;
 
-    if (this.isGameTerminated()) return;
+    if (this.isGameTerminated()) return; // Don't do anything if the game's over
 
     var vector     = this.getVector(direction);
     var traversals = this.buildTraversals(vector);
     var moved      = false;
 
-    console.log(vector);
-
     // Save the current tile positions and remove merger information
     this.prepareTiles();
 
     // Traverse the grid in the right direction and move tiles
+    traversals.x.forEach(function (x) {
+      traversals.y.forEach(function (y) {
+        var cell = { x: x, y: y };
+        var tile = self.grid.cellContent(cell);
+
+        if (tile) {
+          var positions = self.findFarthestPosition(cell, vector);
+          var next      = self.grid.cellContent(positions.next);
+
+          // Only one merger per row traversal?
+          if (next && next.type === tile.type && !next.mergedFrom) {
+            var merged = new Tile(positions.next, next.value + tile.value, tile.type);
+            merged.mergedFrom = [tile, next];
+
+            self.grid.insertTile(merged);
+            self.grid.removeTile(tile);
+
+            // Converge the two tiles' positions
+            tile.updatePosition(positions.next);
+
+            // Update the score
+            //self.score += merged.value;
+          } else {
+            self.moveTile(tile, positions.farthest);
+          }
+
+          if (!self.positionsEqual(cell, tile)) {
+            moved = true; // The tile moved from its original cell!
+          }
+        }
+      });
+    });
 
     // Extract a packed knowledge and a vaccine
+    traversals.x.forEach(function (x) {
+      traversals.y.forEach(function (y) {
+        var cell = { x: x, y: y };
+        var tile = self.grid.cellContent(cell);
+        if (!tile) return;
+
+        if (tile.value >= 30 && tile.type === self.copeWith) {
+          tile.syringe = true;
+          self.won = true;
+          console.log("Finish developing vaccine: " + tile.type);
+        } else if (tile.value >= 20 && tile.type !== self.copeWith) {
+          tile.pack = true;
+          console.log("Share knowledge: " + tile.type);
+        }
+      })
+    });
 
     if (moved) {
       this.addRandomTile();
 
       if (!this.movesAvailable()) {
-        this.over = true;
+        this.over = true; // Game over!
+        console.log("game over");
       }
 
       this.actuate();
+
+      // When tiles are merged, there are some extra tiles under the merged tile,
+      // so we need to remove them but css transition duration is about 200ms (
+      // position movement is about 100ms but pop animation takes 200ms), and so
+      // we should actuate the html renderer after 250ms.
+      setTimeout(function () {
+        self.prepareTiles();
+        self.actuate();
+      }, 250);
     }
   }
 
@@ -171,6 +241,10 @@ class GameManager {
       }
     }
     return false;
+  }
+
+  positionsEqual(a, b) {
+    return a.x === b.x && a.y === b.y;
   }
 }
 
