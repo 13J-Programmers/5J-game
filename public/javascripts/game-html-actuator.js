df909b4681601af28d147019c8daefb23a62ab51
Mako <mnfeconicu41@gmail.com>
2017-07-22 10:47:05 +0900

    remove unused code

--- public/javascripts/game-html-actuator.js
+++ public/javascripts/game-html-actuator.js
@@ -1,9 +1,6 @@
 
 class HTMLActuator {
   constructor() {
-    // this.tileContainer        = $(".tile-container");
-    // this.messageContainer     = $(".game-message");
-    // this.tileRemovedContainer = $(".tile-removed-container");
     this.tileContainer        = document.querySelector(".tile-container");
     this.messageContainer     = document.querySelector(".game-message");
     this.tileRemovedContainer = document.querySelector(".tile-removed-container");
@@ -16,7 +13,6 @@ class HTMLActuator {
 
     window.requestAnimationFrame(() => {
       // Clear tile container
-      // self.tileContainer.empty();
       while (self.tileContainer.firstChild) {
         self.tileContainer.removeChild(self.tileContainer.firstChild);
       }
@@ -33,7 +29,6 @@ class HTMLActuator {
         column.forEach((tile) => {
           if (tile && tile.pack) {
             grid.removeTile(tile); // update grid
-            self.removeTile(tile); // animate
           }
         });
       });
@@ -51,25 +46,10 @@ class HTMLActuator {
   addTile(tile) {
     var self = this;
 
-    // var wrapper   = $("<div></div>");
-    // var inner     = $("<div></div>");
     var wrapper   = document.createElement("div");
     var inner     = document.createElement("div");
     var position  = tile.previousPosition || { x: tile.x, y: tile.y };
     var positionClass = this.cssPositionClass(position);
-
-    // wrapper.addClass("tile");
-    // if (tile.syringe) {
-    //   wrapper.addClass("tile-" + tile.cssType + "-syringe");
-    // } else if (tile.pack) {
-    //   wrapper.addClass("tile-" + tile.cssType + "-pack");
-    // } else {
-    //   wrapper.addClass("tile-" + tile.cssType + "-" + tile.value);
-    // }
-    // wrapper.addClass(positionClass);
-    //
-    // inner.addClass("tile-inner");
-    // // inner.textContent = tile.value;
 
     // We can't use classlist because it somehow glitches when replacing classes
     var classes = ["tile"];
@@ -77,10 +57,6 @@ class HTMLActuator {
       classes.push("tile-" + tile.cssType + "-syringe");
     } else if (tile.pack) {
       classes.push("tile-" + tile.cssType + "-pack");
-    } else if (tile.type === "virus") {
-      classes.push("tile-" + GameManager.cssColorMap[this.metadata.copeWith] + "-virus");
-    } else if (tile.type === "virus-cry") {
-      classes.push("tile-" + GameManager.cssColorMap[this.metadata.copeWith] + "-virus-cry");
     } else {
       classes.push("tile-" + tile.cssType + "-" + tile.value);
     }
@@ -96,12 +72,10 @@ class HTMLActuator {
     if (tile.previousPosition) {
       // Make sure that the tile gets rendered in the previous position first
       window.requestAnimationFrame(function () {
-        // wrapper.addClass( self.cssPositionClass({ x: tile.x, y: tile.y }) );
         classes[2] = self.cssPositionClass({ x: tile.x, y: tile.y });
         self.applyClasses(wrapper, classes); // Update the position
       });
     } else if (tile.mergedFrom) {
-      // wrapper.addClass("tile-merged");
       classes.push("tile-merged");
       this.applyClasses(wrapper, classes);
 
@@ -110,17 +84,14 @@ class HTMLActuator {
         self.addTile(merged);
       });
     } else {
-      // wrapper.addClass("tile-new");
       classes.push("tile-new");
       this.applyClasses(wrapper, classes);
     }
 
     // Add the inner part of the tile to the wrapper
-    // wrapper.append(inner);
     wrapper.appendChild(inner);
 
     // Put the tile on the board
-    // this.tileContainer.append(wrapper);
     this.tileContainer.appendChild(wrapper);
   }
 
@@ -142,19 +113,4 @@ class HTMLActuator {
   }
 
   clearMessage() {}
-
-  removeTile(pos) {
-    // $(".tile-container").find("." + this.cssPositionClass(pos)).each(function(index, elem) {
-    //   if ($(elem).hasClass("tile-merged")) {
-    //     $(elem).appendTo(".tile-removed-container");
-    //   } else {
-    //     $(elem).animate({ opacity: 0 }, 500).promise().then(function () { $(this).remove() })
-    //   }
-    // });
-    //
-    // $(".tile-removed-container .tile").each(function(index, elem) {
-    //   $(elem).css({ zIndex: "100", position: "fixed" })
-    //   $(elem).animate({ top: 0 }, 1000).animate({ opacity: 0 }, 300).promise().then(function () { $(this).remove() })
-    // })
-  }
 }
