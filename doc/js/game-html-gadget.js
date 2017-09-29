90b43e995f7a7499ca91bffdf3c8a4f86e4368f5
Mako <mnfeconicu41@gmail.com>
2017-09-29 16:55:09 +0900

    update syringe color when vaccine is created

--- doc/js/game-html-gadget.js
+++ doc/js/game-html-gadget.js
@@ -19,8 +19,12 @@ class HTMLGadget {
       };
     });
 
+    var player1Syringe = document.querySelector('.game-panel.player1 .syringe .background');
+    var player2Syringe = document.querySelector('.game-panel.player2 .syringe .background');
+
     // When developed vaccine
     this.gameEvent.on('create-vaccine', (type) => {
+      // Hide virus
       var types = ['red', 'yellow', 'green', 'blue'];
       for (var i = 0; i < types.length; i++) {
         var type_i = types[i];
@@ -31,10 +35,29 @@ class HTMLGadget {
         }
       }
 
+      // Show game clear if all vaccine are created.
       var isCreatedAllVaccines = Utils.hash2array(this.createdVaccines).every(x => x);
       if (isCreatedAllVaccines) {
         console.log("game clear!!");
         this.gameEvent.emit('game-clear')
+      }
+
+      // Update syringe color to highlight
+      if (type === 'red') {
+        player1Syringe.style.borderTopColor  = 'rgba(255,192,203,1)';
+        player1Syringe.style.borderLeftColor = 'rgba(255,192,203,1)';
+      }
+      else if (type === 'yellow') {
+        player1Syringe.style.borderRightColor  = 'rgba(255,255,0,1)';
+        player1Syringe.style.borderBottomColor = 'rgba(255,255,0,1)';
+      }
+      else if (type === 'green') {
+        player2Syringe.style.borderTopColor  = 'rgba(144,238,144,1)';
+        player2Syringe.style.borderLeftColor = 'rgba(144,238,144,1)';
+      }
+      else if (type === 'blue') {
+        player2Syringe.style.borderRightColor  = 'rgba(173,216,230,1)';
+        player2Syringe.style.borderBottomColor = 'rgba(173,216,230,1)';
       }
     });
 
