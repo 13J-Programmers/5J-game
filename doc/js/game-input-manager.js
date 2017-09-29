0e0dd217d511896fd6434069ea2a7919b0e85bef
Mako <mnfeconicu41@gmail.com>
2017-09-29 13:51:45 +0900

    listen Gamepad input

--- doc/js/game-input-manager.js
+++ doc/js/game-input-manager.js
@@ -2,10 +2,15 @@
 class InputManager extends EventEmitter {
   constructor(playerID) {
     super();
-    this.listen(playerID);
+    this.listenKey(playerID);
+
+    this.gamepadPressed = false; // Avoid that player keeps button pressing.
+    setInterval(() => {
+      this.listenGamepad(playerID);
+    }, 50);
   }
 
-  listen(playerID) {
+  listenKey(playerID) {
     var self = this;
 
     var mapping;
@@ -44,9 +49,74 @@ class InputManager extends EventEmitter {
         self.restart.call(self, event);
       }
     });
+  }
 
-    // Respond to swipe events
-    // TODO:
+  listenGamepad(playerID) {
+    var gamepadID = playerID - 1;
+    var gamepads = navigator.getGamepads ? navigator.getGamepads()
+                 : navigator.webkitGetGamepads ? navigator.webkitGetGamepads
+                 : [];
+    if (!gamepads) return;
+    if (gamepads.length < playerID) return;
+
+    // Gamepad
+    //
+    //        |-1              1
+    //        |            0  (X)  3
+    //    ----+----x      (Y)     (A)
+    //   -1   |    1          (B)
+    //        y 1              2
+    //
+
+    var gp = gamepads[gamepadID];
+    // console.log(gp);
+    var xAxis = gp.axes[0].toFixed(4);
+    var yAxis = gp.axes[1].toFixed(4);
+
+    if (this.gamepadPressed) {
+      if (this.gamepadNokeyPressed(gp)) {
+        this.gamepadPressed = false;
+      }
+      return;
+    }
+
+    if (yAxis <= -1 || this.gamepadButtonPressed(gp.buttons[1])) {
+      this.emit("move", 0);
+      this.gamepadPressed = true;
+    }
+    else if (xAxis >= 1 || this.gamepadButtonPressed(gp.buttons[3])) {
+      this.emit("move", 1);
+      this.gamepadPressed = true;
+    }
+    else if (yAxis >= 1 || this.gamepadButtonPressed(gp.buttons[2])) {
+      this.emit("move", 2);
+      this.gamepadPressed = true;
+    }
+    else if (xAxis <= -1 || this.gamepadButtonPressed(gp.buttons[0])) {
+      this.emit("move", 3);
+      this.gamepadPressed = true;
+    }
+
+  }
+
+  gamepadNokeyPressed(gp) {
+    var xAxis = gp.axes[0].toFixed(4);
+    var yAxis = gp.axes[1].toFixed(4);
+    return (
+      Math.round(xAxis) === 0 &&
+      Math.round(yAxis) === 0 &&
+      !this.gamepadButtonPressed(gp.buttons[0]) &&
+      !this.gamepadButtonPressed(gp.buttons[1]) &&
+      !this.gamepadButtonPressed(gp.buttons[2]) &&
+      !this.gamepadButtonPressed(gp.buttons[3])
+    )
+  }
+
+  gamepadButtonPressed(b) {
+    if (typeof(b) == "object") {
+      return b.pressed;
+    }
+    return b == 1.0;
   }
 
   restart(event) {
