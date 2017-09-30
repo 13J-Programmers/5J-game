fa2feb24263ced7656fdffc52447c9c955ecc249
Mako <mnfeconicu41@gmail.com>
2017-09-30 15:01:20 +0900

    create Utils.shuffle(array)

--- doc/js/game-utils.js
+++ doc/js/game-utils.js
@@ -2,7 +2,8 @@
 class Utils {
   static rand(min, max) {
     if (max === undefined) {
-      max = min; min = 0;
+      max = min;
+      min = 0;
     }
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
@@ -15,5 +16,21 @@ class Utils {
       values[i] = obj[keys[i]];
     }
     return values;
+  }
+
+  // Sample n random values from a array using the modern version of the
+  // [Fisher-Yates shuffle](http://en.wikipedia.org/wiki/Fisher–Yates_shuffle).
+  // If n is not specified, returns a single random element.
+  static shuffle(array) {
+    var sample = array.slice(0);
+    var length = sample.length;
+    var last = length - 1;
+    for (var index = 0; index < length; index++) {
+      var rand = Utils.rand(index, last);
+      var temp = sample[index];
+      sample[index] = sample[rand];
+      sample[rand] = temp;
+    }
+    return sample;
   };
 }
