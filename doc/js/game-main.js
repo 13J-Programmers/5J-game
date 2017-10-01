2f86288658debd07a558ca5e535226f48226bb0e
Mako <mnfeconicu41@gmail.com>
2017-10-01 11:23:14 +0900

    fix event propagation bugs

--- doc/js/game-main.js
+++ doc/js/game-main.js
@@ -6,7 +6,6 @@ var gameEvent = new GameEvent();
 var intro;
 
 window.addEventListener('load', () => {
-  gameEvent.emit('game-title');
   intro = gameIntro();
 
   // --- Init Puzzle ---
@@ -86,104 +85,139 @@ window.addEventListener('load', () => {
     window.progress = progress;
   }
 
-  // gameEvent.emit('game-start');
-});
 
-
-// --- Vaccines ---
-var createdVaccines = {
-  red:    false,
-  blue:   false,
-  yellow: false,
-  green:  false,
-};
-gameEvent.on('create-vaccine', (type) => {
-  if (['red', 'blue', 'yellow', 'green'].indexOf(type) == -1) {
-    throw 'Unexpected type';
-  }
-  createdVaccines[type] = true;
-  // Emit game-clear if all vaccine are created.
-  var isCreatedAllVaccines = Utils.values(this.createdVaccines).every(x => x);
-  if (isCreatedAllVaccines) {
-    console.log("game clear!!");
-    this.gameEvent.emit('game-clear')
-  }
-});
-gameEvent.on('game-reset', () => {
-  createdVaccines = {
+  // --- Vaccines ---
+  var createdVaccines = {
     red:    false,
     blue:   false,
     yellow: false,
     green:  false,
   };
-});
-
-
-// Press ESC key to reset game
-document.addEventListener('keydown', function (event) {
-  if (event.which === 27) { // ESC
-    gameEvent.emit('game-reset-transition');
-  }
-});
-
-
-gameEvent.on('game-title', () => {
-  var gameTitle = document.querySelector('.game-title');
-  gameTitle.style.opacity = 1;
-  document.addEventListener('keyup', function (event) {
-    if (event.which === 39) {
-      gameTitle.style.opacity = 0;
-      gameTitle.style.transition = 'opacity 500ms ease';
-      gameTitle.addEventListener('transitionend', () => {
-        gameEvent.emit('game-intro');
-        console.log("emit game-intro");
-      }, { once: true })
+  gameEvent.on('create-vaccine', (type) => {
+    if (['red', 'blue', 'yellow', 'green'].indexOf(type) == -1) {
+      throw 'Unexpected type';
     }
-  }, { once: true });
-});
-
-gameEvent.on('game-intro', () => {
-  intro.start().oncomplete(() => {
-    gameEvent.emit('game-countdown');
+    createdVaccines[type] = true;
+    // Emit game-clear if all vaccine are created.
+    var isCreatedAllVaccines = Utils.values(createdVaccines).every(x => x);
+    if (isCreatedAllVaccines) {
+      console.log("game clear!!");
+      this.gameEvent.emit('game-clear')
+    }
   });
-});
-gameEvent.on('game-reset', () => {
-  intro.exit();
-});
+  gameEvent.on('game-reset', () => {
+    createdVaccines = {
+      red:    false,
+      blue:   false,
+      yellow: false,
+      green:  false,
+    };
+  });
 
-gameEvent.on('game-countdown', () => {
+
+  // Press ESC key to reset game
+  document.addEventListener('keydown', function (event) {
+    if (event.which === 27) { // ESC
+      gameEvent.emit('game-reset-transition');
+    }
+  });
+
+
+  gameEvent.on('game-title', () => {
+    var gameTitle = document.querySelector('.game-title');
+    gameTitle.style.opacity = 1;
+    gameTitle.style.transition = 'opacity 500ms ease';
+
+    document.addEventListener('keyup', listener);
+
+    function listener(event) {
+      if (event.which === 39) { // Right arrow
+        gameTitle.style.opacity = 0;
+        gameTitle.addEventListener('transitionend', () => {
+          gameEvent.emit('game-intro');
+          console.log('emit: game-intro');
+        }, { once: true });
+        document.removeEventListener('keyup', listener);
+      }
+      else if (event.which === 27) { // ESC
+        gameTitle.style.opacity = 0;
+        document.removeEventListener('keyup', listener);
+      }
+    }
+  });
+
+
+  gameEvent.on('game-intro', () => {
+    intro.start()
+    intro.oncomplete(() => {
+      gameEvent.emit('game-countdown');
+    });
+  });
+  gameEvent.on('game-reset', () => {
+    intro.exit();
+  });
+
+  var timeout;
   var gameCountdown = document.querySelector('.game-countdown');
   var gameCountdownText = document.querySelector('.game-countdown .count');
-  gameCountdown.style.opacity = 1;
-  setTimeout(() => { gameCountdownText.innerText = '4' }, 1000);
-  setTimeout(() => { gameCountdownText.innerText = '3' }, 2000);
-  setTimeout(() => { gameCountdownText.innerText = '2' }, 3000);
-  setTimeout(() => { gameCountdownText.innerText = '1' }, 4000);
-  setTimeout(() => {
-    gameCountdownText.innerText = 'Start!';
-    gameEvent.emit('game-start');
+  gameCountdown.style.transition = 'opacity 500ms ease';
+  gameEvent.on('game-countdown', () => {
+    gameCountdown.style.opacity = 1;
+    timeout = setTimeout(() => {
+      gameCountdownText.innerText = '4';
+      timeout = setTimeout(() => {
+        gameCountdownText.innerText = '3';
+        timeout = setTimeout(() => {
+          gameCountdownText.innerText = '2';
+          timeout = setTimeout(() => {
+            gameCountdownText.innerText = '1';
+            timeout = setTimeout(() => {
+              gameCountdownText.innerText = 'Start!';
+              console.log('emit: game-start');
+              gameEvent.emit('game-start');
+              gameCountdown.style.opacity = 0;
+              setTimeout(() => {
+                gameCountdownText.innerText = '5'; // for next game
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  });
+  gameEvent.on('game-reset', () => {
+    clearTimeout(timeout);
     gameCountdown.style.opacity = 0;
-    gameCountdown.style.transition = 'opacity 500ms ease';
-  }, 5000);
-  setTimeout(() => { gameCountdownText.innerText = '5' }, 7000); // for next game
-});
+    setTimeout(() => {
+      gameCountdownText.innerText = '5'; // for next game
+    }, 1000);
+  });
 
 
-gameEvent.on('game-reset-transition', () => {
-  // Animation
-});
+  gameEvent.on('game-reset-transition', () => {
+    setTimeout(() => {
+      gameEvent.emit('game-reset');
+    }, 1000);
+  });
 
-gameEvent.on('game-reset', () => {
-  var gameLoadingWrapper = document.querySelector('.game-loading-wrapper');
-  gameLoadingWrapper.style.opacity = 1;
-  gameLoadingWrapper.style.transition = 'opacity 500ms ease';
-  $('.game-loading').circleProgress({
-    size: 200,
-    startAngle: -Math.PI / 2,
-    value: 1.0,
-    animation: { duration: 4000, easing: "circleProgressEasing" },
-  }).on('circle-animation-end', () => {
-    gameEvent.emit('game-title');
-    gameLoadingWrapper.style.opacity = 0;
-  })
+  gameEvent.on('game-reset-transition', () => {
+    // Transition Animation
+    var gameLoadingWrapper = document.querySelector('.game-loading-wrapper');
+    gameLoadingWrapper.style.opacity = 1;
+    gameLoadingWrapper.style.transition = 'opacity 500ms ease';
+    $('.game-loading').circleProgress({
+      size: 200,
+      startAngle: -Math.PI / 2,
+      value: 1.0,
+      animation: { duration: 4000, easing: "circleProgressEasing" },
+    }).on('circle-animation-end', function () {
+      // console.log('emit: game-title');
+      gameEvent.emit('game-title');
+      gameLoadingWrapper.style.opacity = 0;
+      $(this).unbind('circle-animation-end');
+    });
+  });
+
+
+  gameEvent.emit('game-title');
 });
